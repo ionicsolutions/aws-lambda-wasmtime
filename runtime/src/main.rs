a59ec6da7181854689c9efb9a4f79bcedf7795fc
Kilian Kluge <dev@kluge.ai>
2024-11-22 22:42:13 +0100

    fix: merge code into a single file

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,13 +1,10 @@
-use std::env;
-
 use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
 use serde_json::Value;
-use wasmtime::component::{Component, Linker};
-use wasmtime::Engine;
-use wasmtime_wasi::add_to_linker_sync;
+use wasmtime::component::{bindgen, Component, Linker, ResourceTable};
+use wasmtime::{Engine, Store};
+use wasmtime_wasi::{add_to_linker_sync, WasiCtx, WasiCtxBuilder, WasiView};
 
-mod wasm_handler;
-use wasm_handler::{function_handler, BasicState, LambdaFunctionPre};
+include!(concat!(env!("OUT_DIR"), "/bindgen_macro.rs"));
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -18,8 +15,8 @@ async fn main() -> Result<(), Error> {
     let mut linker = Linker::<BasicState>::new(&engine);
     add_to_linker_sync(&mut linker)?;
 
-    let lambda_task_root = env::var("LAMBDA_TASK_ROOT").unwrap_or_else(|_| ".".to_string());
-    let component_file = env::var("_HANDLER")?;
+    let lambda_task_root = std::env::var("LAMBDA_TASK_ROOT").unwrap_or_else(|_| ".".to_string());
+    let component_file = std::env::var("_HANDLER")?;
     let component_path = format!("{}/{}", lambda_task_root, component_file);
     let component = Component::from_file(&engine, component_path)?;
 
@@ -32,4 +29,47 @@ async fn main() -> Result<(), Error> {
         function_handler(shared_engine, shared_pre_instantiated_component, event).await
     }))
     .await
+}
+
+async fn function_handler(
+    engine: &Engine,
+    pre_instantiated_component: &LambdaFunctionPre<BasicState>,
+    event: LambdaEvent<Value>,
+) -> Result<Value, Error> {
+    let ctx = WasiCtxBuilder::new()
+        .inherit_stdout()
+        .inherit_stderr()
+        .build();
+
+    let mut store = Store::new(
+        engine,
+        BasicState {
+            ctx,
+            table: ResourceTable::new(),
+        },
+    );
+
+    let bindings = pre_instantiated_component.instantiate(&mut store)?;
+    let lambda = bindings.component_function_lambda();
+
+    let event: exports::component::function::lambda::Event = serde_json::from_value(event.payload)?;
+
+    let response = lambda.call_handler(&mut store, event)?;
+
+    let response_value = serde_json::to_value(response)?;
+    Ok(response_value)
+}
+
+pub(crate) struct BasicState {
+    ctx: WasiCtx,
+    table: ResourceTable,
+}
+
+impl WasiView for BasicState {
+    fn ctx(&mut self) -> &mut WasiCtx {
+        &mut self.ctx
+    }
+    fn table(&mut self) -> &mut ResourceTable {
+        &mut self.table
+    }
 }
